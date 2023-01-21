c1ad1b49b27825077f6c9bdff554ab60ca3b85f2
Mohosin999 <hasanmohosin998@gmail.com>
2023-01-21 11:34:24 +0600

    ticket

--- api/app/app.js
+++ api/app/app.js
@@ -1,7 +1,16 @@
+require("dotenv").config({ path: "/custom/path/to/.env" });
+// require("dotenv").config("../.env");
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
+// main application
 const app = express();
 
+// middleware
+app.use([morgan("dev"), cors(), express.json()]);
+
+// route handling
 app.get("/health", (_req, res) => {
   res.status(200).json({ message: "Success" });
 });
